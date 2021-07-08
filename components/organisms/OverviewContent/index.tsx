aa23c77a48143081589bd0e8223ed37dfd5d9860
Prawito Hudoro <prawitohudoro@gmail.com>
2021-07-09 05:22:12 +0700

    integrasi member transactions

--- components/organisms/OverviewContent/index.tsx
+++ components/organisms/OverviewContent/index.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { HistoryTransactionTypes, TopUpCategoriesTypes } from '../../../services/data-types';
-import { getMemberOverview } from '../../../services/player';
+import { getMemberOverview } from '../../../services/member';
 import Categori from './Categori';
 import TableRow from './TableRow';
 
--- components/organisms/TransactionContent/TableRow.tsx
+++ components/organisms/TransactionContent/TableRow.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link';
 import cx from 'classnames';
+import NumberFormat from 'react-number-format';
 
 interface TableRowProps {
     image: string;
     title: string;
     category: string;
-    item: number;
+    item: string;
     price: number;
-    status: 'Pending' | 'Success' | 'Failed'
+    status: string;
 }
 export default function TableRow(props: TableRowProps) {
   const {
@@ -15,16 +16,16 @@ export default function TableRow(props: TableRowProps) {
   } = props;
   const statusClass = cx({
     'float-start icon-status': true,
-    pending: status === 'Pending',
-    success: status === 'Success',
-    failed: status === 'Failed',
+    pending: status === 'pending',
+    success: status === 'success',
+    failed: status === 'failed',
   });
   return (
     <tr data-category="pending" className="align-middle">
       <th scope="row">
         <img
           className="float-start me-3 mb-lg-0 mb-3"
-          src={`/img/${image}.png`}
+          src={image}
           width="80"
           height="60"
           alt=""
@@ -39,12 +40,19 @@ export default function TableRow(props: TableRowProps) {
       <td>
         <p className="fw-medium color-palette-1 m-0">
           {item}
-          {' '}
-          Gold
         </p>
       </td>
       <td>
-        <p className="fw-medium color-palette-1 m-0">{price}</p>
+        <p className="fw-medium color-palette-1 m-0">
+          <NumberFormat
+            value={price}
+            prefix="Rp. "
+            displayType="text"
+            thousandSeparator="."
+            decimalSeparator=","
+          />
+
+        </p>
       </td>
       <td>
         <div>
